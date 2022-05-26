90e502fc29eeaf0f4aee862f244c072dd1dd7013
cyrian simard <cyriansimard@gmail.com>
2022-05-26 12:05:38 +0200

    feat(home): add html code to home page

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -5,12 +5,30 @@ function photographerFactory(data) {
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+        const link = document.createElement('a');
+        link.setAttribute("href", '#')
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
+        img.setAttribute("alt", 'name')
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+
+        const paragraphCity = document.createElement('p');
+        paragraphCity.textContent = 'Ville';
+
+        const paragraphTagline = document.createElement('p');
+        paragraphTagline.textContent = 'tagline';
+
+        const paragraphPrice = document.createElement('p');
+        paragraphPrice.textContent = 'price€/jour'
+
+        article.appendChild(link);
+        link.appendChild(img);
+        link.appendChild(h2);
+
+        article.appendChild(paragraphCity);
+        article.appendChild(paragraphTagline);
+        article.appendChild(paragraphPrice);
         return (article);
     }
     return { name, picture, getUserCardDOM }
